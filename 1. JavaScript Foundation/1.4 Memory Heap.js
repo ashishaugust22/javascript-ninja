52b071b427601de18e643f667bcdde23f7884599
Ashish Sharma <attach4ashish@gmail.com>
2021-01-10 07:26:54 +0530

    memory leakage

--- 1. JavaScript Foundation/1.4 Memory Heap.js
+++ 1. JavaScript Foundation/1.4 Memory Heap.js
@@ -6,3 +6,15 @@ const obj = { name: 'Ashish' };
 // so 4, 'some string', {name: 'Ashish'} are gets stored in memory heap
 
 // memory heap is the randomly accessed memory
+
+
+
+
+
+
+// how to fill memory heap
+
+let arr = [];
+for (let i = 5; i > 1; i++) {
+    arr.push(i);
+}
