0cf5dae8129aa37ed36862d0d1c2450b48772a92
Ashish Sharma <attach4ashish@gmail.com>
2021-01-11 08:17:16 +0530

    const/let does not hoist

---
+++ 2. JavaScript Foundation II/2.3 Hoisting.js
@@ -0,0 +1,77 @@
+// 1. variables get hoisted to the top partially. Meaning with no values; (undefined)
+
+// 2. function gets hoisted with definition.
+
+// 3. Hoisting happens for every execution context
+
+
+conole.log(sing()); // works 
+console.log(sing2()); // won't work => sing2 is not defined or undefined
+
+// function declaration
+function sing() {
+    console.log('oh la la la');
+}
+
+// function expression
+function sing2() {
+    console.log('uuuh la la la');
+}
+
+console.log(sing2()); // works
+
+
+
+
+// ===============================================
+
+
+a(); // output  => 'bye'
+
+function a() {
+    console.log('hi');
+}
+
+function a() {
+    console.log('bye');
+}
+
+
+
+// ===============================================
+
+
+
+var favFood = 'Mango';
+
+function foodThoughts() {
+    console.log('Original: ' + favFood);
+
+    var favFood = 'Mango';
+    console.log('New fav food: ' + favFood);
+}
+
+foodThoughts();
+
+// think ?????????????????????????
+
+
+
+// ===============================================
+
+
+var favFood = 'Mango';
+
+function foodThoughts() {
+    console.log('Original: ' + favFood);
+    console.log('New fav food: ' + favFood);
+}
+
+foodThoughts();
+
+
+// ===============================================
+
+
+// use const or let to avoid hoisting
+// JS will throw error instead of undefined with above tow
