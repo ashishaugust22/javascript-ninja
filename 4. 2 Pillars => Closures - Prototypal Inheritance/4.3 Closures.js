da7184340ee13fcdf15c2dc246674061df802507
Ashish Sharma <attach4ashish@gmail.com>
2021-01-20 23:07:57 +0530

    closures

---
+++ 4. 2 Pillars => Closures - Prototypal Inheritance/4.3 Closures.js
@@ -0,0 +1,36 @@
+/*
+1. It is a feature in JS
+2. it is important where function is written
+    not where it is invoked
+*/
+
+function a() {
+    let grandpa = 'grandpa';
+    return function b() {
+        let father = 'father';
+        return function c() {
+            let son = 'son';
+            return `${grandpa} > ${father} > ${son}`;
+        };
+    };
+}
+
+a();
+
+//closures and higher order function
+function boo(string) {
+    return function (name) {
+        return function (name2) {
+            console.log(`hi ${name2}`);
+        };
+    };
+}
+
+const boo2 = (string) => (name) => (name2) => console.log(`hi ${name2}`);
+
+boo('hi')('john')('tanya');
+
+// AHH! HOW DOES IT REMEMBER THIS 5 years from now?
+booString = boo2('sing');
+booStringName = booString('John');
+booStringNameName2 = booStringName('tanya');
