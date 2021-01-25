3107d41a6692adcb2a3edfb90a8e02e65d6bbf82
Ashish Sharma <attach4ashish@gmail.com>
2021-01-25 23:29:57 +0530

    public vs private

---
+++ 5. Object Oriented Programming/5.8 Class Inheritance.js
@@ -0,0 +1,36 @@
+class Character {
+    constructor (name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack () {
+        return 'atack with ' + this.weapon;
+    }
+}
+
+class Elf extends Character {
+    constructor (name, weapon, type) {
+        // console.log('what am i?', this); this gives an error
+        super(name, weapon);
+        console.log('what am i?', this);
+        this.type = type;
+    }
+}
+
+class Ogre extends Character {
+    constructor (name, weapon, color) {
+        super(name, weapon);
+        this.color = color;
+    }
+    makeFort () { // this is like extending our prototype.
+        return 'strongest fort in the world made';
+    }
+}
+
+const houseElf = new Elf('Dolby', 'cloth', 'house');
+//houseElf.makeFort() // error
+const shrek = new Ogre('Shrek', 'club', 'green');
+shrek.makeFort();
+
+
+// instanceof
