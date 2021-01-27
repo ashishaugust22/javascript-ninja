f315b887606b66d85347fcbc02a3aceec18241fe
Ashish Sharma <attach4ashish@gmail.com>
2021-01-27 17:11:16 +0530

    Threads, Concurrency, Parellelism

---
+++ 8. Asynchronous JS/8.11 Threads, Concurrency, Parellelism.js
@@ -0,0 +1,7 @@
+/*
+    https://www.internalpointers.com/post/gentle-introduction-multithreading
+
+    https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
+
+    https://www.freecodecamp.org/news/scaling-node-js-applications-8492bd8afadc/
+*/
