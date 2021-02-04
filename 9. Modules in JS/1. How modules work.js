3be91260288e25a9d30fd6f41cafce81eaae77ef
Ashish Sharma <attach4ashish@gmail.com>
2021-02-04 23:26:12 +0530

    how modules work in JS

---
+++ 9. Modules in JS/1. How modules work.js
@@ -0,0 +1,18 @@
+// IIFE 
+
+const variable = (function () {
+    const privateVariable = "Ashish";
+    const privateFunction = function () {
+        console.log(privateVariable);
+    };
+    return {
+        funnyFunction: privateFunction
+    };
+})();
+
+
+
+/*
+now we can use it in other files
+without polluting the global namespace
+*/
