82532676bf3e24a217a7870b8e140e18ee3eda4a
Ashish Sharma <attach4ashish@gmail.com>
2021-02-23 23:01:42 +0530

    UI changes in list

--- 0. Toy Apps/10. snake game/snake.js
+++ 0. Toy Apps/10. snake game/snake.js
@@ -45,7 +45,6 @@ export function update () {
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] };
     }
-
     snakeBody[0].x += inputDirection.x;
     snakeBody[0].y += inputDirection.y;
 }
